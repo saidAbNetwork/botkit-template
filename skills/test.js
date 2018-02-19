12044ef3f5dd7fed050e594585e2bc2c9db03f93
saidAbNetwork <36502426+saidAbNetwork@users.noreply.github.com>
2018-02-19 11:42:43 +0100

    Add files via upload

---
+++ skills/test.js
@@ -0,0 +1,15 @@
+module.exports = function (controller) {
+
+    controller.hears(['test'], 'direct_message,direct_mention', function (bot, message) {
+
+        bot.startConversation(message, function (err, convo) {
+            convo.say('Create a ticket');
+
+            convo.ask('What is your ticket about?', function (response, convo) 
+            {
+                convo.say('ticket about' + response.text + ' is created!');
+                convo.next();
+            });
+        });
+    });
+};
