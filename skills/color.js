9495f1bcd627da6865e4582e4a3acf373b2c670c
saidAbNetwork <36502426+saidAbNetwork@users.noreply.github.com>
2018-02-20 14:36:51 +0100

    Update color.js

--- skills/color.js
+++ skills/color.js
@@ -1,12 +1,21 @@
 module.exports = function (controller) {
 
-    controller.hears([/^color$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^test$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
-            convo.say('This is a BotKit conversation sample.');
-
-            convo.ask('What is your favorite color?', function (response, convo) {
-                convo.say("Cool, I like '" + response.text + "' too!");
+            convo.say('Create a Ticket.');
+        
+            convo.ask('What is your ticket about?', function (response, convo) 
+            {
+                if(response.text != null)
+                {
+                    convo.ask('Please give a short description: ', function (rep, convo) 
+                    {
+                        convo.next();
+                    });
+                    
+                    convo.say('ticket about ' + response.text + ' is created!');
+                }            
                 convo.next();
             });
         });
